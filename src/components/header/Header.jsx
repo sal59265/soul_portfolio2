a4634f68c764ea145f1cbf5144b5e6844de5ea1f
Soul Lee <lee.soul12@gmail.com>
2022-03-14 19:05:25 -0400

    make a button that downloads the resume in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Soul Lee</h1>
+        <h5 className="text-light">Fullstack Software Engineer</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
