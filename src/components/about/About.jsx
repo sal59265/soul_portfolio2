eeabec61940018eccc297c30b3da794706835dec
Soul Lee <lee.soul12@gmail.com>
2022-03-15 13:31:29 -0400

    finish about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {BiAward} from 'react-icons/bi'
+import {AiFillFolderOpen} from 'react-icons/ai'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className='about__me'>
+          <div className="about__me-image">
+            <img src={ME} alt='about image' />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <BiAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Years</small>
+            </article>
+            <article className='about__card'>
+              <AiFillFolderOpen className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem et dicta adipisci deserunt, libero similique aspernatur? Exercitationem at, dolorum quasi iste numquam similique eius ullam error accusamus natus in alias.
+          </p>
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {useState} from 'react'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBookAlt} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {TiContacts} from 'react-icons/ti'
 
+
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#")
   return (
     <nav>
-      <a href='#'><AiOutlineHome /></a>
-      <a href='#about'><AiOutlineUser /></a>
-      <a href='#experience'><BiBookAlt /></a>
-      <a href='#services'><RiServiceLine /></a>
-      <a href='#contact'><TiContacts /></a>
+      <a href='#' onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} ><BiBookAlt /></a>
+      <a href='#services' onClick={()=> setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''} ><RiServiceLine /></a>
+      <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''} ><TiContacts /></a>
     </nav>
   )
 }
