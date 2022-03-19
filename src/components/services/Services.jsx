4b7f69b9fd981c1944556c7c8671d3da3bc584a4
Soul Lee <lee.soul12@gmail.com>
2022-03-19 14:27:18 -0400

    finish skills section. Still need some work

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <section id="services">Services</section >
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className='container services__container'>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section >
   )
 }
 
