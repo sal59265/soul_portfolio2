7d6ce81abbeec708e9a55188abe1711b11d84764
Soul Lee <lee.soul12@gmail.com>
2022-09-30 11:32:46 -0400

    update the portfolio

--- src/components/Testimonial/Testimonial.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-// const Testimonial = () => {
-//   return (
-//     <section id ="testimonials">Testimonial</section>
-//   )
-// }
-
-// export default Testimonial
